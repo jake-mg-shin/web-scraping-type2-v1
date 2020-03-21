ea21d77f07cae50adac5d2f64d365399627da401
jake-mg-shin <60331338+jake-mg-shin@users.noreply.github.com>
2020-03-21 17:24:08 -0600

    Revert "Initial commit"
    This reverts commit 4ce1b352864cfc355e9d5e274fe8db35b442765f.

--- app.js
+++ app.js
@@ -2,119 +2,17 @@ const axios = require("axios"),
   cheerio = require("cheerio"),
   fs = require("fs");
 
-loop = () => {
-  for (let i = 1; i < 25; i++) {
-    const writeStream = fs.createWriteStream(
-      "volunteerConnector" + [i] + ".csv"
-    );
+const writeStream = fs.createWriteStream("fromInformAlberta.csv");
 
-    const url1 =
-      "https://www.volunteerconnector.org/organizations/page-" + [i] + "/";
-    const url2 = "https://www.volunteerconnector.org";
-
-    let links1 = [];
-    let links2 = [];
-
-    // Write header on CSV
-    writeStream.write(`volunteerConnector \n`);
-
-    getData = async () => {
-      await links2.forEach(link => {
-        axios
-          .get(link)
-          .then(res => {
-            // console.log(res);
-            const $ = cheerio.load(res.data);
-            const $bodyList = $("div.profile");
-
-            $bodyList.each(function(i, el) {
-              const name = $(this)
-                .find("div.follow")
-                .next("h1.name")
-                .text();
-              const address = $(this)
-                .find("div.address")
-                .text();
-              const phone = $(this)
-                .find("div.phone")
-                .text();
-              const email = $(this)
-                .find("div.email")
-                .text();
-              const website = $(this)
-                .find("div.website")
-                .text();
-
-              console.log(name, address, phone, email, website);
-              // Write row to CSV
-              writeStream.write(
-                `${name}, ${address}, ${phone}, ${email}, ${website} \n`
-              );
-            });
-          })
-          .catch(err => {
-            // console.log(err);
-            console.log("getData request err");
-          });
-      });
-    };
-
-    makeUrl = () => {
-      links1.forEach(link => {
-        const _link = url2 + link;
-        // console.log(_link);
-
-        links2.push(_link);
-      });
-      console.log(links2);
-
-      getData();
-    };
-
-    getLinks = async () => {
-      await axios
-        .get(url1)
-        .then(res => {
-          const $ = cheerio.load(res.data);
-          const $bodyList = $("div.is-parent");
-
-          $bodyList.each(function(i, el) {
-            const link = $(this)
-              .find("a")
-              .attr("href");
-
-            // console.log(link);
-            links1.push(link);
-          });
-          console.log(links1);
-        })
-        .then(() => {
-          makeUrl();
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    };
-
-    init = () => {
-      getLinks();
-    };
-
-    init();
-  }
-};
-// loop();
-
-const writeStream = fs.createWriteStream("volunteerConnector1.csv");
-
-const url1 = "https://www.volunteerconnector.org/organizations/page-1/";
-const url2 = "https://www.volunteerconnector.org";
+const url1 =
+  "https://informalberta.ca/public/results/relatedSubjects.do?taxonomyQueryId=2526";
+const url2 = "https://informalberta.ca/public/";
 
 let links1 = [];
 let links2 = [];
 
 // Write header on CSV
-writeStream.write(`volunteerConnector \n`);
+writeStream.write(`fromInformAlberta \n`);
 
 getData = async () => {
   await links2.forEach(link => {
@@ -123,31 +21,24 @@ getData = async () => {
       .then(res => {
         // console.log(res);
         const $ = cheerio.load(res.data);
-        const $bodyList = $("div.profile");
+        const $bodyList = $("div#orgContact");
 
         $bodyList.each(function(i, el) {
           const name = $(this)
-            .find("div.follow")
-            .next("h1.name")
+            .find("div#corpInfoHeading")
             .text();
           const address = $(this)
-            .find("div.address")
+            .find("div#address")
             .text();
-          const phone = $(this)
-            .find("div.phone")
+          const contact = $(this)
+            .find("div#telephone")
             .text();
-          const email = $(this)
-            .find("div.email")
-            .text();
-          const website = $(this)
-            .find("div.website")
+          const info = $(this)
+            .find("div.expandableDataArea")
             .text();
 
-          console.log(name, address, phone, email, website);
           // Write row to CSV
-          writeStream.write(
-            `${name}, ${address}, ${phone}, ${email}, ${website} \n`
-          );
+          writeStream.write(`${name}, ${address}, ${contact}, ${info} \n`);
         });
       })
       .catch(err => {
@@ -159,7 +50,7 @@ getData = async () => {
 
 makeUrl = () => {
   links1.forEach(link => {
-    const _link = url2 + link;
+    const _link = url2 + link.substring(3);
     // console.log(_link);
 
     links2.push(_link);
@@ -174,7 +65,7 @@ getLinks = async () => {
     .get(url1)
     .then(res => {
       const $ = cheerio.load(res.data);
-      const $bodyList = $("div.is-parent");
+      const $bodyList = $("span.solLink");
 
       $bodyList.each(function(i, el) {
         const link = $(this)
